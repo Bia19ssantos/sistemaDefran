8153ff724718a2bf988f53e85f493a2b605aa6c8
Bia19ssantos <bia19.ssantos@gmail.com>
2026-06-12 14:18:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,16 +12,16 @@ col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     st.markdown("""<div style="background-color:#007bff; color:white; padding:15px; border-radius:5px;">
-    <b>Orçamento</b><br>Novo ></div>""", unsafe_allow_html=True)
+    <b></b><br> ></div>""", unsafe_allow_html=True)
 with col2:
     st.markdown("""<div style="background-color:#ffc107; color:black; padding:15px; border-radius:5px;">
-    <b>Vendas</b><br>Adicionar ></div>""", unsafe_allow_html=True)
+    <b></b><br> ></div>""", unsafe_allow_html=True)
 with col3:
     st.markdown("""<div style="background-color:#28a745; color:white; padding:15px; border-radius:5px;">
-    <b>Pedidos</b><br>Novo ></div>""", unsafe_allow_html=True)
+    <b></b><br> ></div>""", unsafe_allow_html=True)
 with col4:
     st.markdown("""<div style="background-color:#dc3545; color:white; padding:15px; border-radius:5px;">
-    <b>Clientes</b><br>Ir agora ></div>""", unsafe_allow_html=True)
+    <b></b><br> ></div>""", unsafe_allow_html=True)
 
 st.markdown("---") # Linha divisória
 
